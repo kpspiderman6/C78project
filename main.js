39a1899594015dc0ad1f4747b51e372f5f176820
kpspiderman6 <73602349+kpspiderman6@users.noreply.github.com>
2020-10-30 10:41:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+document.getElementById("img1").style.visibility = "hidden";
+document.getElementById("button1").style.visibility = "hidden";
+document.getElementById("img2").style.visibility = "hidden";
+document.getElementById("button2").style.visibility = "hidden";
+document.getElementById("img3").style.visibility = "hidden";
+document.getElementById("button3").style.visibility = "hidden";
+document.getElementById("img4").style.visibility = "hidden";
+document.getElementById("button4").style.visibility = "hidden";
+function hidding(){
+    document.getElementById("img1").style.visibility = "hidden";
+    document.getElementById("button1").style.visibility = "hidden";
+    document.getElementById("img2").style.visibility = "visible";
+    document.getElementById("button2").style.visibility = "visible";
+    document.getElementById("img3").style.visibility = "hidden";
+    document.getElementById("button3").style.visibility = "hidden";
+    document.getElementById("img4").style.visibility = "hidden";
+    document.getElementById("button4").style.visibility = "hidden";
+    document.getElementById("h11").style.visibility = "hidden";
+}
+function hidding2(){
+    document.getElementById("img2").style.visibility = "hidden";
+    document.getElementById("button2").style.visibility = "hidden";
+    document.getElementById("img3").style.visibility = "visible";
+    document.getElementById("button3").style.visibility = "visible";
+    document.getElementById("img4").style.visibility = "hidden";
+    document.getElementById("button4").style.visibility = "hidden";
+    document.getElementById("img1").style.visibility = "hidden";
+    document.getElementById("button1").style.visibility = "hidden";
+    document.getElementById("h12").style.visibility = "hidden";
+}
+function hidding3(){
+    document.getElementById("img3").style.visibility = "hidden";
+    document.getElementById("button3").style.visibility = "hidden";
+    document.getElementById("img4").style.visibility = "visible";
+    document.getElementById("button4").style.visibility = "visible";
+    document.getElementById("img2").style.visibility = "hidden";
+    document.getElementById("button2").style.visibility = "hidden";
+    document.getElementById("img1").style.visibility = "hidden";
+    document.getElementById("button1").style.visibility = "hidden";
+    document.getElementById("h13").style.visibility = "hidden";
+}
+function hidding4(){
+    document.getElementById("img4").style.visibility = "hidden";
+    document.getElementById("button4").style.visibility = "hidden";
+    document.getElementById("img2").style.visibility = "hidden";
+    document.getElementById("button2").style.visibility = "hidden";
+    document.getElementById("img3").style.visibility = "hidden";
+    document.getElementById("button3").style.visibility = "hidden";
+    document.getElementById("img1").style.visibility = "hidden";
+    document.getElementById("button1").style.visibility = "hidden";
+    document.getElementById("h14").style.visibility = "hidden";
+}
